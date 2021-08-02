1b4776561a26ebdd8f78c22bfe2b0da8f9078950
Karina Shkatula <me.janedales@gmail.com>
2021-08-02 10:32:32 +0300

    add recycler view multiple types

---
+++ app/src/main/java/com/example/firstapp/ui/calls/CallsActivity.java
@@ -0,0 +1,73 @@
+package com.example.firstapp.ui.calls;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.firstapp.R;
+
+import java.util.ArrayList;
+
+public class CallsActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calls);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+
+        ArrayList<Employee> employees = new ArrayList<>();
+
+        Employee employee = new Employee();
+        employee.setName("Robert");
+        employee.setAddress("New York");
+        employee.setPhone("+61234456");
+        employees.add(employee);
+
+        employee = new Employee();
+        employee.setName("Tom");
+        employee.setAddress("California");
+        employee.setEmail("tom_frank@gmail.com");
+        employees.add(employee);
+
+        employee = new Employee();
+        employee.setName("Smith");
+        employee.setAddress("Philadelphia");
+        employee.setEmail("smith_carrol@gmail.com");
+        employees.add(employee);
+
+        employee = new Employee();
+        employee.setName("Ryan");
+        employee.setAddress("Canada");
+        employee.setPhone("+612001456");
+        employees.add(employee);
+
+        employee = new Employee();
+        employee.setName("Mark");
+        employee.setAddress("Boston");
+        employee.setEmail("mark_walmerd@gmail.com");
+        employees.add(employee);
+
+        employee = new Employee();
+        employee.setName("Adam");
+        employee.setAddress("Brooklyn");
+        employee.setPhone("+61211780");
+        employees.add(employee);
+
+        employee = new Employee();
+        employee.setName("Kevin");
+        employee.setAddress("New Jersey");
+        employee.setPhone("+94221035");
+        employees.add(employee);
+
+        // set adapter
+        EmployeeAdapter adapter = new EmployeeAdapter(this, employees);
+        recyclerView.setAdapter(adapter);
+    }
+
+}
